7e88c47ea702ce75358aa5bdf531c845a462f6c9
Matteo <mattecalautti@gmail.com>
2025-07-24 17:55:45 +0200

    Centralizzazione paths

---
+++ Scripts/my_paths.py
@@ -0,0 +1,55 @@
+##--------------- Info bici --------------------------------------------#
+
+# RAW
+PATH_CICLABILI_RAW = "../Data/Raw/Info_bici/bike_ciclabili_raw.geojson"
+PATH_BIKEMI_RAW = '../Data/Raw/Info_bici/bikemi_stazioni_raw.csv'
+
+# CLEAN
+PATH_CICLABILI_CLEAN = "../Data/Clean/Info_bici/bike_ciclabili_clean.geojson"
+PATH_BIKEMI_CLEAN = "../Data/Clean/Info_bici/bikemi_stazioni_clean.geojson"
+
+#------------------------------------------------------------------------
+
+#----------------------------- Punti di interesse ------------------------#
+
+#PATH RAW
+PATH_MUNICIPI_RAW = "../Data/Raw/Punti_di_interesse/Municipi/Municipi.shp"
+PATH_PARCHI_RAW = "../Data/Raw/Punti_di_interesse/Parchi/Parchi_WGS84_Milano_1.shp"
+PATH_FONTANE_RAW = "../Data/Raw/Punti_di_interesse/fontane_raw.csv"
+PATH_BIBLIOTECHE_RAW = "../Data/Raw/Punti_di_interesse/biblioteche_raw.geojson"
+PATH_IMPIANTI_SPORTIVI_RAW = "../Data/Raw/Punti_di_interesse/impianti_sportivi_raw.geojson"
+#   Scuole
+PATH_SCUOLE_PRIMARIE_RAW = "../Data/Raw/Istruzione/Scuole/scuole-primarie_raw.geojson"
+PATH_SCUOLE_SECONDARIE_PRIMO_RAW = "../Data/Raw/Istruzione/Scuole/scuole-secondarie-primo-grado_raw.geojson"
+PATH_SCUOLE_SECONDARIE_SECONDO_RAW = "../Data/Raw/Istruzione/Scuole/scuole-secondarie-secondo-grado_raw.geojson"
+PATH_UNIVERSITA_RAW = "../Data/Raw/Istruzione/Scuole/universita_raw.geojson"
+
+#PATH CLEAN
+PATH_MUNICIPI_CLEAN = "../Data/Clean/Punti_di_interesse/municipi_clean.geojson"
+PATH_PARCHI_CLEAN = "../Data/Clean/Punti_di_interesse/parchi_clean.geojson"
+PATH_FONTANE_CLEAN = "../Data/Clean/Punti_di_interesse/fontane_clean.geojson"
+PATH_BIBLIOTECHE_CLEAN = "../Data/Clean/Punti_di_interesse/biblioteche_clean.geojson"
+PATH_IMPIANTI_SPORTIVI_CLEAN = "../Data/Clean/Punti_di_interesse/impianti_sportivi_clean.geojson"
+PATH_SCUOLE_CLEAN = "../Data/Clean/Istruzione/Scuole/scuole_clean.geojson"
+
+#-----------------------------------------------------------------------------------------------#
+
+#--------------------------------------- Inquinamento ------------------------------------------#
+
+# RAW
+PATH_INQUINAMENTO_INGESTION_RAW = "../Data/Raw/Inquinamento/Airnet/inquinamento-airnet_media_raw.geojson"
+
+# CLEAN
+PATH_INQUINAMENTO_INGESTION_CLEAN = "../Data/Clean/Inquinamento/Airnet/inquinamento-airnet_ingestion_clean.geojson"
+
+#-----------------------------------------------------------------------------------------------#
+
+#--------------------------------------- Strade ------------------------------------------#
+
+# RAW
+PATH_STRADE_URBANE_RAW = "../Data/Raw/Strade_urbane/avgLST_Milan_UrbanAtlas/avgLST_Milan_UrbanAtlas.shp"
+
+# CLEAN
+PATH_STRADE_URBANE_CLEAN = "../Data/Clean/Strade_urbane/strade-urbane_clean.geojson"
+
+#-----------------------------------------------------------------------------------------------#
