6b67b172e2942d81cb5cd744fe01a524b4240ba6
Matteo <mattecalautti@gmail.com>
2025-08-06 12:19:54 +0200

    Refactoring Repository

--- Scripts/my_paths.py
+++ Scripts/my_paths.py
@@ -1,18 +1,27 @@
 ##--------------- Info bici --------------------------------------------#
 
 # RAW
-PATH_CICLABILI_RAW = "../Data/Raw/Info_bici/bike_ciclabili_raw.geojson"
-PATH_BIKEMI_RAW = '../Data/Raw/Info_bici/bikemi_stazioni_raw.csv'
+PATH_CICLABILI_RAW = "../Data/Raw/Rete_ciclabile_stradale/bike_ciclabili_raw.geojson"
+PATH_STRADE_CICLABILI_PICKLE_RAW = "../Data/Raw/Rete_ciclabile_stradale/strade_ciclabili.pickle"
+
+# STAGING
+        #Ciclabili
+PATH_CICLABILI_PICKLE_STAGING = "../Data/Staging/Rete_ciclabile_stradale/ciclabile.pickle"
+PATH_CICLABILI_GEOJSON_STAGING = "../Data/Staging/Rete_ciclabile_stradale/ciclabile.geojson"
+        #Strade
+PATH_STRADE_CICLABILI_PICKLE_STAGING = "../Data/Staging/Rete_ciclabile_stradale/strade_ciclabili.pickle"
+PATH_STRADE_CICLABILI_GEOJSON_STAGING = "../Data/Staging/Rete_ciclabile_stradale/strade_ciclabili.geojson"
 
 # CLEAN
-PATH_CICLABILI_CLEAN = "../Data/Clean/Info_bici/bike_ciclabili_clean.geojson"
-PATH_BIKEMI_CLEAN = "../Data/Clean/Info_bici/bikemi_stazioni_clean.geojson"
+PATH_RETE_CICLABILE_COMPLETA_PICKLE_CLEAN = "../Data/Clean/Rete_ciclabile_stradale/rete_ciclabile_completa.pickle"
+PATH_RETE_CICLABILE_COMPLETA_GEOJSON_CLEAN = "../Data/Clean/Rete_ciclabile_stradale/rete_ciclabile_completa.geojson"
 
 #------------------------------------------------------------------------
 
 #----------------------------- Punti di interesse ------------------------#
 
 #PATH RAW
+PATH_BIKEMI_RAW = '../Data/Raw/Punti_di_interesse/bikemi_stazioni_raw.csv'
 PATH_MUNICIPI_RAW = "../Data/Raw/Punti_di_interesse/Municipi/Municipi.shp"
 PATH_PARCHI_RAW = "../Data/Raw/Punti_di_interesse/Parchi/Parchi_WGS84_Milano_1.shp"
 PATH_FONTANE_RAW = "../Data/Raw/Punti_di_interesse/fontane_raw.csv"
@@ -32,6 +41,7 @@ PATH_SCUOLE_SECONDARIE_SECONDO_RAW = "../Data/Raw/Istruzione/Scuole/scuole-secon
 PATH_UNIVERSITA_RAW = "../Data/Raw/Istruzione/Scuole/universita_raw.geojson"
 
 #PATH CLEAN
+PATH_BIKEMI_CLEAN = "../Data/Clean/Punti_di_interesse/bikemi_stazioni_clean.geojson"
 PATH_MUNICIPI_CLEAN = "../Data/Clean/Punti_di_interesse/municipi_clean.geojson"
 PATH_PARCHI_CLEAN = "../Data/Clean/Punti_di_interesse/parchi_clean.geojson"
 PATH_FONTANE_CLEAN = "../Data/Clean/Punti_di_interesse/fontane_clean.geojson"
@@ -58,15 +68,13 @@ PATH_INQUINAMENTO_INGESTION_CLEAN = "../Data/Clean/Inquinamento/Airnet/inquiname
 
 #-----------------------------------------------------------------------------------------------#
 
-#--------------------------------------- Strade ------------------------------------------#
-
-# RAW
-PATH_STRADE_URBANE_RAW = "../Data/Raw/Strade_urbane/avgLST_Milan_UrbanAtlas/avgLST_Milan_UrbanAtlas.shp"
-
-# CLEAN
-PATH_STRADE_URBANE_CLEAN = "../Data/Clean/Strade_urbane/strade-urbane_clean.geojson"
+#-------------------------------------- Analisi ----------------------------------------------#
+PATH_SPORT_TEMPO_LIBERO_ANALISI_PICKLE_STAGING = "../Data/Staging/Analisi/sport_tempo_libero_clean.geojson"
+PATH_SPORT_TEMPO_LIBERO_ANALISI_CLEAN = "../Data/Clean/Analisi/sport_tempo_libero_clean.geojson"
 
 #-----------------------------------------------------------------------------------------------#
+
+#----------------------------- CRS (lo so non sono dei path) ----------------------------------------#
 
 
 CRS_GRAD = "EPSG:4326"  # Lat-Long
